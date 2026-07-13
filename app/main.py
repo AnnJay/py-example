10bae7958d9356ca8274d11cadd747edab199ec9
ann-jay-dev <ann-jay@inbox.ru>
2026-07-13 14:19:32 +0300

    set cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine
@@ -8,6 +9,16 @@ from .router import post, user, auth, vote
 models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
@@ -15,6 +26,6 @@ app.include_router(auth.router)
 app.include_router(vote.router)
 
 
-@ app.get("/")
+@app.get("/")
 async def root():
     return {"message": "Welcome to my api"}
